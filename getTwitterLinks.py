8ae9765b96e270ac29334767cc20a3e311388d0a
gwsears <gerzel@foxpaws.net>
2016-05-16 11:43:57 -0400

    Added TODOs and cleaned up some testing code.

--- getTwitterLinks.py
+++ getTwitterLinks.py
@@ -18,9 +18,14 @@ AccessTokenSecret = 'Your Secret Here'
 ConsumerKey = 'Your Key Here'
 ConsumerSecret = 'Your Secret Here'
 
+#  Target settings
+accountName = 'timoreilly'
+numbToGet = 30
+
 def getTweets(account,tweets):
     # Gets a number of status updates for a particular account.
     # account is the screen name you want, and tweets is the number of updates.
+    # Returns a twitter status object with 1 or more tweets.
     # TODO: Find a way to set this up to pull tweets by date range.
     t = Twitter(auth=OAuth(AccessToken,AccessTokenSecret,ConsumerKey,ConsumerSecret))
     pythonStatuses = t.statuses.user_timeline(screen_name=account, count=tweets)
@@ -28,16 +33,34 @@ def getTweets(account,tweets):
 
 def getURLs(tweetsGot):
     # Searches each tweet for embedded URLs.  At the moment this only picks up the first URL.
-    # TODO: Add support for multiple URLs.
-    # TODO: Add return of URLs in list or dictionary possibly with the tweet they were found in.
     # TODO: Add URL checking.
+    listURLs = []  # entered as [{'text':'tweet text', 'urlsFound':Boolean, urls:['URL1', 'URL2']}]
     for index in range(len(tweetsGot)):
-        print(tweetsGot[index]['text'])
+        #print(tweetsGot[index]['text'])
+        listURLs.append({'text':tweetsGot[index]['text']})
         search = re.search("(?P<url>https?://[^\s]+)", tweetsGot[index]['text'])
         if search == None:
-            print("No URLs Found.")
+            #print("No URLs Found.")
+            listURLs[index]['urlsFound'] = False
+            listURLs[index]['urls'] = []
         else:
-            print("Found: " + re.search("(?P<url>https?://[^\s]+)", tweetsGot[index]['text']).group("url"))
+            listURLs[index]['urlsFound'] = True
+            foundURLs = re.findall("(?P<url>https?://[^\s]+)", tweetsGot[index]['text'])
+            #print(foundURLs)
+            listURLs[index]['urls'] = foundURLs
+            #print("Found: " + re.search("(?P<url>https?://[^\s]+)", tweetsGot[index]['text']).group("url"))
+    #print(listURLs)
+    return listURLs
 
-tweetsGot = getTweets('timoreilly',30)
-getURLs(tweetsGot)
+tweetsGot = getTweets(accountName,numbToGet)
+print(tweetsGot)
+urlsGot = getURLs(tweetsGot)
+print("* * * * Here's the URLs pulled with each tweet. * * * *")
+print(urlsGot)
+
+for index in range(len(urlsGot)):
+    print('This tweet: ' + urlsGot[index]['text'])
+    if urlsGot[index]['urlsFound'] == True:
+        print('  has the following URLs: ' + str(urlsGot[index]['urls']))
+    else:
+        print('  has no URLs.')
