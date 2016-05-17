677041ebf9c3ea02f09decac7800f12bf7f967d6
gwsears <gerzel@foxpaws.net>
2016-05-17 10:50:21 -0400

    Added function to search for Emails instead of URLs.

--- getTwitterLinks.py
+++ getTwitterLinks.py
@@ -7,7 +7,10 @@
 """
 # TODO: Add cli interface to program.
 # TODO: Add exception/error handling.
-# TODO: Add export urls to file, perhaps cvs.
+# TODO: Add export urls/emails to file, perhaps csv.
+# TODO: Build function to print out results.
+# TODO: Add GUI interface.
+# TODO: Setup as stand-alone program.
 
 # Using Twitter Tools from http://mike.verdone.ca/twitter/#downloads
 from twitter import Twitter, OAuth
@@ -20,8 +23,8 @@ ConsumerKey = 'Your Key Here'
 ConsumerSecret = 'Your Secret Here'
 
 #  Target settings
-accountName = 'timoreilly'
-numbToGet = 30
+accountName = 'geraldwsears'
+numbToGet = 3
 
 def getTweets(account,tweets):
     # Gets a number of status updates for a particular account.
@@ -36,33 +39,48 @@ def getURLs(tweetsGot):
     # Searches each tweet for embedded URLs.  At the moment this only picks up the first URL.
     # TODO: Add URL checking.
     # TODO: Figure out if there is a better way to pull URLs & if I'm getting the whole URL.
+    # TODO: Embedded media isn't getting captured very well.  URLs shortened, need to figure out how to fix.
     listURLs = []  # entered as [{'text':'tweet text', 'urlsFound':Boolean, urls:['URL1', 'URL2']}]
     for index in range(len(tweetsGot)):
-        #print(tweetsGot[index]['text'])
         listURLs.append({'text':tweetsGot[index]['text']})
         search = re.search("(?P<url>https?://[^\s]+)", tweetsGot[index]['text'])
-        if search == None:
-            #print("No URLs Found.")
+        if search == []:
             listURLs[index]['urlsFound'] = False
             listURLs[index]['urls'] = []
         else:
             listURLs[index]['urlsFound'] = True
             foundURLs = re.findall("(?P<url>https?://[^\s]+)", tweetsGot[index]['text'])
-            #print(foundURLs)
             listURLs[index]['urls'] = foundURLs
-            #print("Found: " + re.search("(?P<url>https?://[^\s]+)", tweetsGot[index]['text']).group("url"))
-    #print(listURLs)
     return listURLs
 
-tweetsGot = getTweets(accountName,numbToGet)
-print(tweetsGot)
-urlsGot = getURLs(tweetsGot)
-print("* * * * Here's the URLs pulled with each tweet. * * * *")
-print(urlsGot)
+def getEmails(tweetsGot):
+    # Searches each tweet for emails contained in the text of the tweet.
+    listEmails = [] # [{'text':'tweet text','emailsFound':Boolean,emails:[emaila,emailb...]}]
+    test = []
+    for index in range(len(tweetsGot)):
+        search = re.findall("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}",tweetsGot[index]['text'])
+        print("This search for index" + str(index))
+        print(search)
+        if search == test:
+            listEmails.append({'text': tweetsGot[index]['text']})
+            listEmails[index]['emailsFound'] = False
+        else:
+            listEmails.append({'text': tweetsGot[index]['text']})
+            listEmails[index]['emailsFound'] = True
+            foundEmails = re.findall("[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}",tweetsGot[index]['text'],re.IGNORECASE)
+            listEmails[index]['emails'] = foundEmails
+    return listEmails
 
-for index in range(len(urlsGot)):
-    print('This tweet: ' + urlsGot[index]['text'])
-    if urlsGot[index]['urlsFound'] == True:
-        print('  has the following URLs: ' + str(urlsGot[index]['urls']))
+tweetsGot = getTweets(accountName,numbToGet)
+emailsGot = getEmails(tweetsGot)
+# This is a check on data being collected with the URLs being pulled by our regular expression.
+print("* * * * Here's the URLs pulled with each tweet. * * * *")
+print(emailsGot)
+
+# This is a printout closer to something useable.
+for index in range(len(emailsGot)):
+    print('This tweet: ' + emailsGot[index]['text'])
+    if emailsGot[index]['emailsFound'] == True:
+        print('  has the following emails: ' + str(emailsGot[index]['emails']))
     else:
-        print('  has no URLs.')
+        print('  has no emails.')
